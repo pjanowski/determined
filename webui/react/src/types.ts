b2c999e26205c6b82db7797599bfc9c91eb3f5a1
Hamid Zare <hamid@determined.ai>
2021-06-29 13:50:28 -0700

    workload states

--- webui/react/src/types.ts
+++ webui/react/src/types.ts
@@ -310,7 +310,6 @@ export interface MetricName {
 
 export interface Checkpoint extends StartEndTimes {
   resources?: Record<string, number>;
-  state: CheckpointState;
   trialId: number;
   uuid? : string;
   validationMetric? : number;
@@ -322,7 +321,6 @@ export interface Workload extends StartEndTimes {
 
 export interface CheckpointWorkload extends Workload {
   resources?: Record<string, number>;
-  state: CheckpointState;
   uuid? : string;
 }
 
@@ -334,7 +332,6 @@ export interface CheckpointWorkloadExtended extends CheckpointWorkload {
 export interface MetricsWorkload extends Workload {
   metrics?: Record<string, number>;
   numInputs?: number;
-  state: RunState;
 }
 export interface WorkloadWrapper {
   checkpoint?: CheckpointWorkload;
